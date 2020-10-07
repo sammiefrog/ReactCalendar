96d8ab84c3bbbe0f00b0a40a64dc991bacdf1930
sammiefrog <sammiefrog@live.com>
2020-10-07 18:07:30 -0500

    brought in register and login pages from old app

---
+++ client/src/Components/RegisterForm/action.js
@@ -0,0 +1,16 @@
+// importing necessary dependency
+import axios from "axios";
+
+// registration information posted by user and exported for other parts of application
+export const SendRegistration = async (username, password) => {
+  try {
+    const reply = axios({
+      method: "POST",
+      url: "/api/user/register",
+      data: { username, password },
+    });
+    return reply;
+  } catch (error) {
+    console.log(error);
+  }
+};
